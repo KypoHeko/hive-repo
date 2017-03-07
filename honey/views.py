8568c80e88cf7b41da6bf136017b06a82f3c2909
KypoHeko <kypo28@gmail.com>
2017-03-08 02:35:01 +0500

    Update for loops, update gallery

--- honey/views.py
+++ honey/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, "index.html")
 
 def main(request):
-    return render(request, "main.html")
+    return render(request, "main.html", {'range':range(1,12)})
 
 def er404(request):
     return render(request, "404.html")
@@ -21,7 +21,7 @@ def communities(request):
     return render(request, "communities.html")
 
 def gallery(request):
-    return render(request, "gallery.html")
+    return render(request, "gallery.html", {'range':range(1,19)})
 
 def id(request, pk):
     data = Persona.objects.get(id=pk)
